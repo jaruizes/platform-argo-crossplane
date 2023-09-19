89e4f2992af17b7a37c30262b46d0ae9f19e881c
Jose Alberto Ruiz Casarrubios <jaruiz@paradigmadigital.com>
2023-09-19 17:32:05 +0200

    Deleted files generated during the demo

--- 04.delete-cluster.sh
+++ 04.delete-cluster.sh
@@ -1,4 +1,23 @@
+git pull .
+
 rm -rf backstage-repository/catalog/generated
-git commit -m "Delete files generated during the demo"
-git push .
+git commit -a -m "Deleted files generated during the demo"
+git push origin main
+
+sleep 10
+
+echo "Removing claims associated to platform tools and CI/CD...."
+rm -rf platform-gitops-repositories/claims/products/platform-tools
+rm -rf platform-gitops-repositories/claims/products/platform-cicd
+git commit -a -m "Deleted tools and cicd claims"
+git push origin main
+sleep 360
+
+echo "Removing claims associated to platform base...."
+rm -rf platform-gitops-repositories/claims/products
+git commit -a -m "Deleted platform claim"
+git push origin main
+sleep 900
+
+
 eksctl delete cluster -f cluster-conf.yaml --disable-nodegroup-eviction
