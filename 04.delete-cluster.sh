7dd16e936eef615968b24a15ae6e85216d683c06
Jose Alberto Ruiz Casarrubios <jaruiz@paradigmadigital.com>
2023-09-19 17:09:24 +0200

    Updated PoC

--- 04.delete-cluster.sh
+++ 04.delete-cluster.sh
@@ -1,1 +1,4 @@
+rm -rf backstage-repository/catalog/generated
+git commit -m "Delete files generated during the demo"
+git push .
 eksctl delete cluster -f cluster-conf.yaml --disable-nodegroup-eviction
